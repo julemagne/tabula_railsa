9b6f101571fa0fe15b508c767c09afcf4eb246aa
Julie David <julie.angela.david@gmail.com>
2015-10-07 10:58:59 -0400

    REST challenge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
 
   def form
-    if request.post?
-      @answer = Answer.create!(answer_params)
-    else
-      @answer = Answer.new
-    end
+    @answer = Answer.new
+  end
+
+  def results
+    @answer = Answer.save(answer_params)
   end
 
   private def answer_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#form'
+
   get 'home/form'
-  post 'home/form'
+  post 'home/results'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
